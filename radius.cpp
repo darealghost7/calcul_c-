97ac1b78678d698ec4d95fee47bbb0cb52f92e93
Daryl <164248137+darealghost7@users.noreply.github.com>
2026-03-04 23:33:39 +0200

    Add files via upload

---
+++ radius.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const double PI = 3.14159;
+    double radius = 20;
+    double circumfrance = 2 * PI * radius;
+
+    cout << "Circumfrance"<< circumfrance << endl;
+
+    return 0;
+}
