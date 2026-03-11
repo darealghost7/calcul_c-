4fe48b221163a1f0632051a0075909bc1b6d650e
Daryl <164248137+darealghost7@users.noreply.github.com>
2026-03-11 11:25:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double length;
+    double width;
+    double area;
+    double perimeter;
+    length = 12;
+    width = 5;
+    perimeter = 2 * (length + width );
+    area = length * width;
+
+    cout << "Program to compute and output the perimeter and area" << endl;
+    cout << "This is thr area of a rectangle" << endl;
+    cout << "Length = " << length << endl;
+    cout << "Area = " << area << endl;
+    cout << "Perimeter = " << perimeter << endl;
+    cout << "Area = " << area << endl;
+    return 0;
+}
